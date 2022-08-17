221384a71f8398ade8ebf572fb8a2fcd794c4af1
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-16 21:10:08 -0700

    got a bunch of files for crud created, got the home and base working

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Snack(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(default=" ")
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Name: {self.title}, Description: {self.description}, Purchaser: {self.purchaser}'
--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Snack
 
-# Create your views here.
+class Homepageview(TemplateView):
+    template_name = 'home.html'
+
+class SnackListView(ListView):
+    template_name = 'snack-list.html'
+    model = Snack
+
+class SnackDetailView(DeleteView):
+    template_name = 'snack-detail.html'
+
+class SnackCreateView(CreateView):
+    template_name = 'snack-create.html'
+
+class SnackUpdateView(UpdateView):
+    template_name = 'snack-update.html'
+
+class SnackDeleteView(DeleteView):
+    template_name = 'snack-delete.html'
